580ab96d2d65965db128685858436e16988eafa4
Animesh Shukla <animeshshukla1518@gmail.com>
2023-04-09 21:31:06 +0530

    connected mongodb to nodejs

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
+const db = "mongodb+srv://animeshshukla1518:<saymyname1518>@cluster0.ch9eohv.mongodb.net/notesdb";
+mongoose.connect(db).then(function(){
+    // home page route
+    app.get("/",function(req,res){
+        res.send("This is the homepage.");
+    });
 
-// home page route
-app.get("/",function(req,res){
-    res.send("This is the homepage.");
-});
-
-// notes page
-app.get("/notes",function(req,res){
-    res.send("This is the notes page.");
-});
+    // notes page
+    app.get("/notes",function(req,res){
+        res.send("This is the notes page.");
+    });
+})
 
 // starting the server
 app.listen(5000,function(){
